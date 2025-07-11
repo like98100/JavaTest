e184ea6f45d9f6a88a6beea16e6b977cd3e71721
ByeongChang <like98100@naver.com>
2025-07-11 16:16:16 +0900

    리스트 예제 추가

---
+++ src/org/example/ListTest.java
@@ -0,0 +1,29 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListTest {
+
+    // method
+    public static void main(String[] args)
+    {
+        List<String> list = new ArrayList<>();
+        list.add("Hello");
+        list.add("World");
+        System.out.println(list);
+        for(int i = 0; i < list.size(); i++)
+        {
+            System.out.println(i + "번째 아이템: " + list.get(i));
+        }
+
+        List<Integer> intList = new ArrayList<>();
+        intList.add(1);
+        System.out.println(intList);
+
+        List<Animal> animalList = new ArrayList<>();
+        animalList.add(new Cat());
+        System.out.println(animalList);
+
+    }
+}
