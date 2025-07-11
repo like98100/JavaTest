bdf11ee0b831697e5386939d351b340eb3073028
ByeongChang <like98100@naver.com>
2025-07-11 15:09:06 +0900

    주석 제거

--- src/org/example/Calculator.java
+++ src/org/example/Calculator.java
@@ -1,12 +1,7 @@
 package org.example;
 
 public class Calculator {
-
-    // 사칙연산 메소드 제작 후 feature 브런치 생성하여 commit/push
-    // git branch feature/calculate(선택) || git checkout -b feature/calculate(선택)
-    // github 화면에서 Pull Request 생성
-    // PR에서 reviewer로 강사님(joo-pe) 지정하기
-    // 리뷰가 끝나면 main에 merge
+    
     // var
 
     // method
