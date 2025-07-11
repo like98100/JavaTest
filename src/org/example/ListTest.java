3c1fe42886f5345516afb6080b0af0ad5b18df7d
ByeongChang <like98100@naver.com>
2025-07-11 16:30:19 +0900

    For문 사용하여 Int List 입출력

--- src/org/example/ListTest.java
+++ src/org/example/ListTest.java
@@ -8,25 +8,33 @@ public class ListTest {
     // method
     public static void main(String[] args)
     {
-        List<String> list = new ArrayList<>();
-        list.add("Hello");
-        list.add("World");
-        System.out.println(list);
-        for(int i = 0; i < list.size(); i++)
-        {
-            System.out.println(i + "번째 아이템: " + list.get(i));
-        }
+//        List<String> list = new ArrayList<>();
+//        list.add("Hello");
+//        list.add("World");
+//        System.out.println(list);
+//        for(int i = 0; i < list.size(); i++)
+//        {
+//            System.out.println(i + "번째 아이템: " + list.get(i));
+//        }
+//
+//        List<Animal> animalList = new ArrayList<>();
+//        animalList.add(new Cat());
+//        animalList.add(new Dog());
+//        for (Animal animal : animalList) {
+//            animal.sound();
 
-        List<Integer> intList = new ArrayList<>();
-        intList.add(1);
-        System.out.println(intList);
-
-        List<Animal> animalList = new ArrayList<>();
-        animalList.add(new Cat());
-        animalList.add(new Dog());
-        for (Animal animal : animalList) {
-            animal.sound();
-        }
+        createIntList(9);
 
     }
-}
+
+    public static void createIntList(int num)
+    {
+        List<Integer> intList = new ArrayList<>();
+
+        for(int i = 0; i < num; i++)  intList.add(i+1);                     // 1 ~ num(9) 저장
+
+        System.out.println(intList);                                        // 리스트 전체 출력
+        for(int i = 0; i < num; i++) System.out.println(intList.get(i));    // for문 사용하여 출력
+    }
+
+    }
