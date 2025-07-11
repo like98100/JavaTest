fa475d6233b1453b732a7321ee70fe9d3bdf1056
ByeongChang <like98100@naver.com>
2025-07-11 10:20:30 +0900

    Method 처리

--- JavaTest/src/org/example/Main.java
+++ JavaTest/src/org/example/Main.java
@@ -1,37 +1,53 @@
 package org.example;
 
 public class Main {
-    public static void main(String[] args) {
-         //System.out.println("Hello world!");
-        System.out.println(10+10);
-        int num1 = 5;
-        int num2 = 7;
 
+    //var
+
+    //method
+    public static void add(int num1, int num2)
+    {
         System.out.println("num1 + num2: "+num1 + num2);
+    }
 
-        int x = 5;
-        x += 3;
-        System.out.println("x: " + x);
+    public static int plus(int num, int plusValue)
+    {
+        num += plusValue;
+        return num;
+    }
 
-        System.out.println(x == 10);
+    public static void equal(int num, int standardNum)
+    {
+        System.out.println(num == standardNum);
+    }
 
-        System.out.println((5>3)&& (10>5));
+    public static void scoreOper(int num)
+    {
+        System.out.println((num >=90) ? "A" : "B");
+    }
 
-        x++;
-        System.out.println("x: " + x);
+    public static void bitOper(int num1, int num2)
+    {
+        System.out.println(num1&num2);
+    }
 
-        int score = 85;
-        System.out.println((score >=90) ? "A" : "B");
+    public static String ifMethod(int num)
+    {
+//        if(num > 0) System.out.println("양수입니다.");
+//        else if(num == 0) System.out.println("0입니다.");
+//        else System.out.println("음수입니다.");
 
-        int n = 5;
-        int m = 3;
-        System.out.println(n&m);
+        String strSum = "";
+        if(num > 0) strSum = "양수";
+        else if(num == 0) strSum = "0";
+        else strSum = "음수";
 
-        if(x < 5) System.out.println("x < 5");
-        else System.out.println("x >= 5");
+        return strSum;
+    }
 
-        int day = 3;
-        switch (day) {
+    public static void switchMethod(int num)
+    {
+        switch (num) {
             case 1:
                 System.out.println("월요일");
                 break;
@@ -45,6 +61,35 @@ public class Main {
                 System.out.println("기타 요일");
                 break;
         }
+    }
+
+    public static void main(String[] args) {
+         //System.out.println("Hello world!");
+        System.out.println(10+10);
+
+        add(5,7);
+
+        int x = 5;
+        System.out.println("x: " + plus(x, 3));
+
+        equal(x, 10);
+
+        System.out.println((5>3) && (10>5));
+
+        x++;
+        System.out.println("x: " + x);
+
+        int score = 85;
+        scoreOper(score);
+
+        int n = 5;
+        int m = 3;
+        bitOper(n, m);
+
+        System.out.println(ifMethod(3) + "입니다.");
+
+        int day = 3;
+        switchMethod(day);
 
         for(int i = 0; i < 3; i++)
         {
@@ -63,5 +108,6 @@ public class Main {
             x++;
             System.out.println("x: " + x);
         } while (x < 25);
+
     }
 }
