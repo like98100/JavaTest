7c2fe78cfec96168e90d364d5f7c4e45fc0b52a6
ByeongChang <like98100@naver.com>
2025-07-11 09:16:08 +0900

    추가 빌드
    동기화

--- JavaTest/src/Main.java
+++ JavaTest/src/Main.java
@@ -10,11 +10,39 @@ public class Main {
         int x = 5;
         x += 3;
         System.out.println("x: " + x);
+
+        System.out.println(x == 10);
+
+        System.out.println((5>3)&& (10>5));
+
         x++;
         System.out.println("x: " + x);
 
+        int score = 85;
+        System.out.println((score >=90) ? "A" : "B");
+
+        int n = 5;
+        int m = 3;
+        System.out.println(n&m);
+
         if(x < 5) System.out.println("x < 5");
         else System.out.println("x >= 5");
+
+        int day = 3;
+        switch (day) {
+            case 1:
+                System.out.println("월요일");
+                break;
+            case 2:
+                System.out.println("화요일");
+                break;
+            case 3:
+                System.out.println("수요일");
+                break;
+            default:
+                System.out.println("기타 요일");
+                break;
+        }
 
         for(int i = 0; i < 3; i++)
         {
